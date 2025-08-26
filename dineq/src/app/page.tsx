686dbf6f6718509e33304e4ef29c92ac7be05a04
Maedot Amha <maedotamha@gmail.com>
2025-08-26 11:10:52 +0300

    input and  buttons

--- dineq/src/app/page.tsx
+++ dineq/src/app/page.tsx
@@ -1,10 +1,14 @@
-import NavBar from "@/components/restaurant/NavBar";
+import NavBar from "@/components/common/NavBar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-     <NavBar role = "OWNER"/>
+     <NavBar role = "CUSTOMER"/>
+     <Input placeholder="input" label = "name" type = "password" required className=""/>
+     <Button variant={"outline"}>Click me</Button>
      <p>Your one-stop solution for restaurant reservations</p>
    </div>
   );
